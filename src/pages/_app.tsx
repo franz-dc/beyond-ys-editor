1310c575ea8f8abb5047e4d0feb319f650cda2a9
Franz Joshua Dela Cruz <franzdc.21@gmail.com>
2023-08-20 21:09:26 +0800

    refactor: remove conditional import for zod

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import { createRef, useEffect } from 'react';
+import { createRef } from 'react';
 
 import { CacheProvider } from '@emotion/react';
 import type { EmotionCache } from '@emotion/react';
@@ -9,14 +9,23 @@ import Head from 'next/head';
 import { ThemeProvider as NextThemeProvider } from 'next-themes';
 import { SnackbarProvider } from 'notistack';
 import { MdClose } from 'react-icons/md';
+import { z } from 'zod';
 
 import { PageProvider } from '~/contexts';
 import { createEmotionCache } from '~/utils';
+
 const clientSideEmotionCache = createEmotionCache();
 
 export interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
 }
+
+z.setErrorMap((issue, ctx) => {
+  if (issue.code === 'too_small' && issue.minimum === 1) {
+    return { message: 'This field is required.' };
+  }
+  return { message: ctx.defaultError };
+});
 
 export default function MyApp(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
@@ -26,22 +35,6 @@ export default function MyApp(props: MyAppProps) {
   const onClickDismiss = (key: any) => () => {
     notistackRef?.current?.closeSnackbar(key);
   };
-
-  useEffect(() => {
-    const initZod = async () => {
-      if (process.env.NODE_ENV === 'development') {
-        const z = await import('zod');
-        z.setErrorMap((issue, ctx) => {
-          if (issue.code === 'too_small' && issue.minimum === 1) {
-            return { message: 'This field is required.' };
-          }
-          return { message: ctx.defaultError };
-        });
-      }
-    };
-
-    initZod();
-  }, []);
 
   return (
     <PageProvider emotionCache={emotionCache}>
